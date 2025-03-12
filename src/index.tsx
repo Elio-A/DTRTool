309ba63ed6a2faf9a192fb5e1a4c81449ca299ce
Elio-A <elio_a@icloud.com>
2025-03-12 17:45:52 -0300

    Testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from "react-dom/client";
 import App from './App'
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
