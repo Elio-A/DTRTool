dc60fc08460d0c7ffc944321f4706a0079b274ea
Elio-A <elio_a@icloud.com>
2025-03-22 11:41:33 -0300

    Started Login Page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import Login from './views/login'
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      <Login/>
     </BrowserRouter>
   </StrictMode>
 )
---
+++ src/views/login.tsx
@@ -0,0 +1,15 @@
+function login(){
+    const handleLogin = async () => {
+
+    }
+
+    return(
+        <div>
+            <h1>
+                Login Page
+            </h1>
+        </div>        
+    )
+}
+
+export default login
