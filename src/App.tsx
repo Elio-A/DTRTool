00a7b115e6d1e07ca19eb01c08b7e02651eba753
Khaled <k9developments@outlook.com>
2025-03-25 10:23:50 -0800

    xx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 import Login from "./views/LoginPage/Login"
 import HomePage from "./views/HomePage/Home"
-import { Route, Routes } from 'react-router-dom';
+// import MapView from "./views/MapPage/MapView"
+// import NbPower from './views/OutsideLinks/NbPower';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Box } from '@mui/material';
+// import Sidebar from './global/AppSidebar';
+import { useState } from 'react';
+//import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 
 function App() {
   console.log("Here")
